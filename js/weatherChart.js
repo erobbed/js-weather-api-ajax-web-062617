b05d2b7886f5074c6152d3d52fb5516b79d22ce7
Ethan Roberts <ethan.reid.roberts@gmail.com>
2017-08-16 14:20:27 -0400

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,37 @@
 function getFahrenheits(result){
-  // Your code goes here
+   const temps = result.hourly_forecast.map(hour => hour.temp.english)
+   return temps
 }
 
 function getHours(result){
-  // Your code goes here
+  const hours = result.hourly_forecast.map(hour => hour.FCTTIME.hour)
+  return hours
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+  var data = {
+    labels: labels,
+    datasets: [{
+      label:"Hourly Weather for New York",
+      fillColor: "rgba(220,220,220,0.2)",
+      strokeColor: "rgba(220,220,220,1)",
+      pointColor: "rgba(220,220,220,1)",
+      pointStrokeColor: "#fff",
+      pointHighlightFill: "#fff",
+      pointHighlightStroke: "rgba(220,220,220,1)",
+      data: data
+    }]
+  }
+  return data
 }
 
 function makeAjaxRequest(endpoint, success) {
-  // Your code goes here
+  // fetch(endpoint)
+  // .then(result => result.text())
+  //   .then(data => success(JSON.parse(data)))
+    $.ajax({
+      url: endpoint,
+      dataType: 'jsonp',
+      success: success
+    })
 }
